a2071498a096b59857ca4726fa2513b1d6ed5e5c
xiter22222 <pawanpediredla@gmail.com>
2024-09-05 14:23:02 +0530

    Added blockchain (MetaMask) authentication, OAuth with Google and GitHub, credential-based sign-in with NextAuth, and Prisma adapter for user handling.

---
+++ .history/components/credentialsForm_20240905142220.tsx
@@ -0,0 +1,12 @@
+export function CredentialsSignInButton() {
+  const handleClick = () => signIn();
+
+  return (
+    <button
+      onClick={handleClick}
+      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
+    >
+      <span className="ml-4">Continue with Email</span>
+    </button>
+  );
+}
