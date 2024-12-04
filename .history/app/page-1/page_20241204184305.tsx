350f9991bba98d3e0f8cfc49810f5b3360c82001
xiter22222 <pawanpediredla@gmail.com>
2024-12-04 18:49:12 +0530

    Fix: Move useRouter to client-side execution to prevent NextRouter not mounted error

--- .history/app/page-1/page_20241204184305.tsx
+++ .history/app/page-1/page_20241204184305.tsx
@@ -1,9 +1,11 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
+
 const CallButtons = dynamic(() => import('@/callbutton/CallButton'), {
   loading: () => <p>Loading...</p>,
   ssr: false,
 });
+
 const Page: React.FC = () => {
   return <CallButtons />;
 };
